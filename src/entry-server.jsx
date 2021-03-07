96f1e19c7be5a17a9b475c14742f5cbf1da0f4fc
takurinton <taryochanmannn@icloud.com>
2021-03-07 16:11:19 +0900

    build ok

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,9 +1,9 @@
-import { render } from 'preact-render-to-string' 
+import { render as r } from 'preact-render-to-string' 
 import { LocationProvider, Router } from 'preact-iso/router';
 import { App } from './App'
 
 export function render(url, context, json) {
-  return render(
+  return r(
     <LocationProvider>
       <Router>
         <App {...json} />
